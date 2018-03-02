eef1fc85ac63c70d0cb6667260d12d8c5445e240
Boram Choi <boram3510@gmail.com>
2018-03-02 18:01:16 +0900

    '.'

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
+
 class Hello {
-    public static void main(String[] args) {
-        System.out.println("hello, world!");
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
     }
-} 
-
-
+}
---
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/App.java
@@ -0,0 +1,7 @@
+package bitcamp.java106.pms; //src main java는 안쓴다
+
+public class App{
+    public static void main(String[]args){
+        System.out.println("Hello world!");
+    }
+}
